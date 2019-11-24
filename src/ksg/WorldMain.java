7508c8ca0b9ed0b3e426a0be819d1f4d9f320a95
Tomek Mrugalski <thomson@klub.com.pl>
2019-11-24 20:44:32 +0100

    TASK 3.3 in progress: colors modified to be more organge

--- src/ksg/WorldMain.java
+++ src/ksg/WorldMain.java
@@ -614,6 +614,7 @@ public class WorldMain implements GLEventListener {
 
             if (kc == KeyEvent.VK_P) {
                 postProcessingActive = !postProcessingActive;
+                System.out.printf("Postprocessing is %s", (postProcessingActive?"TRUE":"FALSE"));
             }
         }
 
--- src/ksg/lab3/shaders/ScreenFragment.glsl
+++ src/ksg/lab3/shaders/ScreenFragment.glsl
@@ -10,4 +10,6 @@ void main(void)
     vec2 posPos = textureCoordinates;
 
     gl_FragColor = texture2D(texture, posPos);
+    gl_FragColor.r += 0.3f;
+    gl_FragColor.g += 0.3f;
 }
