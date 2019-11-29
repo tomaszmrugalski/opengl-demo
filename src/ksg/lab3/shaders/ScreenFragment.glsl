5de515cea2088a781568c74b819c1219c6a72696
Tomek Mrugalski <tomasz@isc.org>
2019-11-29 12:30:15 +0100

    Vertex shader fix (rotating Sun now works)

--- src/ksg/lab3/shaders/ScreenFragment.glsl
+++ src/ksg/lab3/shaders/ScreenFragment.glsl
@@ -13,10 +13,10 @@ void main(void)
     // This changes the x offset based on y and a phase
     // the /200 at the end determines amplitude of the wave
     // the 16*3.14159 determines how many waves are there vertically on screen
-    posPos.x += sin(posPos.y * 16*3.14159 + phase) / 200;
+    textureCoordinates.x += sin(textureCoordinates.y * 16*3.14159 + phase) / 200;
 
     // TASK 3.3: Make the world more orange
-    gl_FragColor = texture2D(texture, posPos);
-    gl_FragColor.r += 0.3f;
-    gl_FragColor.g += 0.3f;
+    gl_FragColor = texture2D(texture, textureCoordinates);
+    gl_FragColor.r += 0.3;
+    gl_FragColor.g += 0.3;
 }
