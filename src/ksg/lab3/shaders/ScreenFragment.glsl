6f88093e696d15702c2f5fa43ae7817e6e6b2f2f
Artur Engler <student@MAC17.local>
2019-11-29 12:47:49 +0100

    fragment shader fixed

--- src/ksg/lab3/shaders/ScreenFragment.glsl
+++ src/ksg/lab3/shaders/ScreenFragment.glsl
@@ -11,12 +11,12 @@ void main(void)
     vec2 posPos = textureCoordinates;
 
     // This changes the x offset based on y and a phase
-    // the /200 at the end determines amplitude of the wave
-    // the 16*3.14159 determines how many waves are there vertically on screen
-    textureCoordinates.x += sin(textureCoordinates.y * 16*3.14159 + phase) / 200;
+    // the /100 at the end determines amplitude of the wave
+    // the 20 determines how many waves are there vertically on screen
+    posPos.x += sin(posPos.y * 20.0 + phase) / 100.0;
 
     // TASK 3.3: Make the world more orange
-    gl_FragColor = texture2D(texture, textureCoordinates);
+    gl_FragColor = texture2D(texture, posPos);
     gl_FragColor.r += 0.3;
     gl_FragColor.g += 0.3;
 }
